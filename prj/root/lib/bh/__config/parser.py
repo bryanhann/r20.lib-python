c63ec1f06ff6e15d11465678ad5cdb31b1c169c4
Bryan Hann <nobody@nowhere>
2020-02-28 15:01:25 +0800

    Remove debris

--- prj/root/lib/bh/__config/parser.py
+++
@@ -1,3 +0,0 @@
-import argparse
-subparser=argparse.ArgumentParser( add_help=False )
-subparser.add_argument( '--config', action='store')
--- prj/root/lib/bh/__main__.py
+++
@@ -1,6 +0,0 @@
-from bh.tools.parse import superparser
-import sys
-parser = superparser(__package__)
-opt = parser.parse_args()
-print( sys.argv )
-print(opt)
